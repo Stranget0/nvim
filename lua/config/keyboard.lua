15e75bf48ca08b4d65e627728ffbbe71335e2e6d
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2024-05-24 20:54:32 +0200

    cmp performance and keys changes

--- lua/config/keyboard.lua
+++ lua/config/keyboard.lua
@@ -1,156 +1,162 @@
 local M = {}
 
 M.keys = {
-	project_list = "<leader>pl",
-	project_add = "<leader>pa",
-	find_file = "<leader>ff",
-	old_files = "<leader>fr",
-	new_file = "<leader>fn",
+  project_list = "<leader>pl",
+  project_add = "<leader>pa",
+  find_file = "<leader>ff",
+  old_files = "<leader>fr",
+  new_file = "<leader>fn",
 
-	next_diagnostic = "g[",
-	prev_diagnostic = "g]",
-	diagnostic_list = "<leader>cd",
-	go_definition = "gd",
-	go_type = "gt",
-	go_references = "gr",
-	rename = "<leader>cr",
-	code_action = "<leader>ca",
-	hover_info = "K",
-	open_docs = "<leader>td",
+  next_diagnostic = "g[",
+  prev_diagnostic = "g]",
+  diagnostic_list = "<leader>cd",
+  go_definition = "gd",
+  go_type = "gt",
+  go_references = "gr",
+  rename = "<leader>cr",
+  code_action = "<leader>ca",
+  hover_info = "<leader>cK",
+  open_docs = "<leader>td",
 
-	git_prev_hunk = "h[",
-	git_next_hunk = "h]",
-	git_stage_hunk = '<leader>gs',
-	git_reset_hunk = '<leader>gr',
-	git_stage_buffer = '<leader>gS',
-	git_undo_stage_hunk = '<leader>gu',
-	git_reset_buffer_hunks = '<leader>gR',
-	git_preview_hunk = '<leader>gp',
-	git_blame_line = '<leader>gb',
-	git_blame_line_toggle = '<leader>gtb',
-	git_diffthis = '<leader>gd',
-	git_diffthis_tilde = '<leader>gD',
-	git_toggle_deleted_hunk = '<leader>gtd',
-	git_select_hunk = 'gih',
+  git_prev_hunk = "<leader>h[",
+  git_next_hunk = "<leader>h]",
+  git_stage_hunk = '<leader>hs',
+  git_reset_hunk = '<leader>hr',
+  git_stage_buffer = '<leader>hS',
+  git_undo_stage_hunk = '<leader>hu',
+  git_reset_buffer_hunks = '<leader>hR',
+  git_preview_hunk = '<leader>hp',
+  git_blame_line = '<leader>hb',
+  git_blame_line_toggle = '<leader>hB',
+  git_diffthis = '<leader>hd',
+  git_diffthis_tilde = '<leader>hD',
+  git_toggle_deleted_hunk = '<leader>hH',
+  git_select_hunk = '<leader>hh',
 
-	cmp_confirm = '<Enter>',
-	cmp_trigger = '<C-Space>',
-	cmp_scroll_up = '<C-u>',
-	cmp_scroll_down = '<C-d>',
-	cmp_jump_forward = '<C-f>',
-	cmp_jump_backward = '<C-b>',
+  cmp_confirm = '<Enter>',
+  cmp_trigger = '<C-Space>',
+  cmp_scroll_up = '<tab>',
+  cmp_scroll_down = '<S-tab>',
+  cmp_jump_forward = '<C-f>',
+  cmp_jump_backward = '<C-b>',
 
-	run_nearest_test = "<leader>rt",
-	run_tests_in_file = "<leader>rT",
+  run_nearest_test = "<leader>rt",
+  run_tests_in_file = "<leader>rT",
 
-	runnables = "<leader>rr",
-	last_runnable = "<leader>rR",
+  runnables = "<leader>rr",
+  last_runnable = "<leader>rR",
 
-	comment = '<C-m>',
-	comment_line = '<C-m>',
-	comment_visual = '<C-m>',
-	comment_textobject = '<C-m>',
+  comment = '<C-m>',
+  comment_line = '<C-m>',
+  comment_visual = '<C-m>',
+  comment_textobject = '<C-m>',
 
 
-	surrounding_add = 'sa',
-	surrounding_delete = 'sd',
-	surrounding_find = 'sf',
-	surrounding_find_left = 'sF',
-	surrounding_highlight = 'sh',
-	surrounding_replace = 'sr',
-	surrounding_update_n_lines = 'sn',
-	surrounding_suffix_last = 'l',
-	surrounding_suffix_next = 'n',
+  surrounding_add = 'sa',
+  surrounding_delete = 'sd',
+  surrounding_find = 'sf',
+  surrounding_find_left = 'sF',
+  surrounding_highlight = 'sh',
+  surrounding_replace = 'sr',
+  surrounding_update_n_lines = 'sn',
+  surrounding_suffix_last = 'l',
+  surrounding_suffix_next = 'n',
 
-	code_move_up = "J",
-	code_move_down = "K",
+  code_move_up = "J",
+  code_move_down = "K",
 
-	operators_evaluate = "g=",
-	operators_exchange = "gx",
-	operators_multiply = "gm",
-	operators_replace = "gr",
-	operators_sort = "gs",
+  operators_evaluate = "<leader>c=",
+  operators_exchange = "<leader>cx",
+  operators_multiply = "<leader>cm",
+  operators_replace = "<leader>cr",
+  operators_sort = "<leader>cs",
 
-	ai_around = 'a',
-	ai_inside = 'i',
-	ai_around_next = 'an',
-	ai_inside_next = 'in',
-	ai_around_last = 'al',
-	ai_inside_last = 'il',
-	ai_goto_left = 'g[',
-	ai_goto_right = 'g]',
+  ai_around = 'a',
+  ai_inside = 'i',
+  ai_around_next = 'an',
+  ai_inside_next = 'in',
+  ai_around_last = 'al',
+  ai_inside_last = 'il',
+  ai_goto_left = 'gh',
+  ai_goto_right = 'gl',
 
-	notifications_history = "<leader>nh",
-	notifications_clear = "<leader>nd",
+  notifications_history = "<leader>nh",
+  notifications_clear = "<leader>nd",
 
-	folds_open_all = 'zR',
-	folds_close_all = 'zM',
+  folds_open_all = 'zR',
+  folds_close_all = 'zM',
 
-	docs_split_remain_focused = "<leader>hs",
-	docs_vsplit_remain_focused = "<leader>hv",
-	docs_split = "<leader>hS",
-	docs_vsplit = "<leader>hV",
+  format_buffer = "<leader>bf",
 
-	format_buffer = "<leader>f",
-
-	code_outline = "<leader>co",
+  code_outline = "<leader>co",
 }
 
 M.triggers = {
-	-- Leader triggers
-	{ mode = 'n', keys = '<Leader>' },
-	{ mode = 'x', keys = '<Leader>' },
+  -- Leader triggers
+  { mode = 'n', keys = '<Leader>' },
+  { mode = 'x', keys = '<Leader>' },
 
-	-- Built-in completion
-	{ mode = 'i', keys = '<C-x>' },
+  -- Built-in completion
+  { mode = 'i', keys = '<C-x>' },
 
-	-- `g` key
-	{ mode = 'n', keys = 'g' },
-	{ mode = 'x', keys = 'g' },
+  -- `g` key
+  { mode = 'n', keys = 'g' },
+  { mode = 'x', keys = 'g' },
 
-	-- `f` key
-	{ mode = 'n', keys = 'f' },
+  -- `f` key
+  { mode = 'n', keys = 'f' },
 
-	-- mini comment / surround
-	{ mode = 'n', keys = 's' },
-	{ mode = 'n', keys = 'l' },
-	{ mode = 'n', keys = 'n' },
+  -- mini comment / surround
+  { mode = 'n', keys = 's' },
+  { mode = 'n', keys = 'l' },
+  { mode = 'n', keys = 'n' },
 
-	-- Marks
-	{ mode = 'n', keys = "'" },
-	{ mode = 'n', keys = '`' },
-	{ mode = 'x', keys = "'" },
-	{ mode = 'x', keys = '`' },
+  -- Marks
+  { mode = 'n', keys = "'" },
+  { mode = 'n', keys = '`' },
+  { mode = 'x', keys = "'" },
+  { mode = 'x', keys = '`' },
 
-	-- Registers
-	{ mode = 'n', keys = '"' },
-	{ mode = 'x', keys = '"' },
-	{ mode = 'i', keys = '<C-r>' },
-	{ mode = 'c', keys = '<C-r>' },
+  -- Registers
+  { mode = 'n', keys = '"' },
+  { mode = 'x', keys = '"' },
+  { mode = 'i', keys = '<C-r>' },
+  { mode = 'c', keys = '<C-r>' },
 
-	-- Window commands
-	{ mode = 'n', keys = '<C-w>' },
+  -- Window commands
+  { mode = 'n', keys = '<C-w>' },
 
-	-- `z` key
-	{ mode = 'n', keys = 'z' },
-	{ mode = 'x', keys = 'z' },
+  -- `z` key
+  { mode = 'n', keys = 'z' },
+  { mode = 'x', keys = 'z' },
 
-	-- LSP
-	{ mode = "n", keys = "[" },
-	{ mode = "n", keys = "]" },
-	{ mode = "n", keys = "g" },
-	{ mode = "n", keys = "g" },
-	{ mode = "n", keys = "g" },
-	{ mode = "n", keys = "g" },
-	{ mode = "n", keys = "L" },
+  -- LSP
+  { mode = "n", keys = "[" },
+  { mode = "n", keys = "]" },
+  { mode = "n", keys = "g" },
+  { mode = "n", keys = "g" },
+  { mode = "n", keys = "g" },
+  { mode = "n", keys = "g" },
+  { mode = "n", keys = "L" },
 }
 
 M.clues = {
-	{ mode = "n", keys = "<leader>c", desc = "+code" },
-	{ mode = "n", keys = "<leader>r", desc = "+runnables" },
-	{ mode = "n", keys = "<leader>h", desc = "+hover docs" },
-	{ mode = "n", keys = "<leader>p", desc = "+projects" },
-	{ mode = "n", keys = "<leader>n", desc = "+notifications" },
+  { mode = "n", keys = "<leader>c", desc = "+code" },
+  { mode = "n", keys = "<leader>b", desc = "+buffer" },
+  { mode = "n", keys = "<leader>r", desc = "+runnables" },
+  { mode = "n", keys = "<leader>p", desc = "+projects" },
+  { mode = "n", keys = "<leader>n", desc = "+notifications" },
+  { mode = "n", keys = "<leader>h", desc = "+git hunk" },
 }
+
+M.check_conflicts = function()
+  for i, key1 in pairs(M.keys) do
+    for j, key2 in pairs(M.keys) do
+      if i ~= j and key1 == key2 then
+        vim.notify("conflicting keys [" .. i .. '] and [' .. j .. "] - " .. key1)
+      end
+    end
+  end
+end
 
 return M
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,149 +1,153 @@
 local icons = require("config.icons")
 local servers = {
-	jsonls = {},
-	dockerls = {},
-	bashls = {},
-	gopls = {},
-	ruff_lsp = {},
-	vimls = {},
-	yamlls = {},
-	wgsl_analyzer = {},
-	eslint = {},
-	lua_ls = {
-		settings = {
-			Lua = {
-				completion = {
-					callSnippet = "Replace"
-				}
-			}
-		}
-	},
-	taplo = {
-		keys = require("config.keymaps").static.taplo,
-	},
+  jsonls = {},
+  dockerls = {},
+  bashls = {},
+  gopls = {},
+  ruff_lsp = {},
+  vimls = {},
+  yamlls = {},
+  wgsl_analyzer = {},
+  eslint = {},
+  lua_ls = {
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = "Replace"
+        }
+      }
+    }
+  },
+  taplo = {
+    keys = require("config.keymaps").static.taplo,
+  },
 }
 
 return {
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		priority = 1,
-		branch = 'v3.x',
-		config = function()
-			require("neodev").setup()
-			local lsp = require('lsp-zero')
-			lsp.extend_lspconfig()
-			local keymaps = require('config.keymaps')
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    priority = 1,
+    branch = 'v3.x',
+    config = function()
+      require("neodev").setup()
+      local lsp = require('lsp-zero')
+      lsp.extend_lspconfig()
+      local keymaps = require('config.keymaps')
 
-			lsp.set_sign_icons({
-				error = icons.diagnostics.Error,
-				warn = icons.diagnostics.Warn,
-				info = icons.diagnostics.Info,
-				hint = icons.diagnostics.Hint,
-			})
+      lsp.set_sign_icons({
+        error = icons.diagnostics.Error,
+        warn = icons.diagnostics.Warn,
+        info = icons.diagnostics.Info,
+        hint = icons.diagnostics.Hint,
+      })
 
-			lsp.on_attach(function(client, bufnr)
-				keymaps.lsp(bufnr)
-			end)
+      lsp.on_attach(function(client, bufnr)
+        keymaps.lsp(bufnr)
+      end)
 
-			require('mason').setup({})
-			local ensure_installed = { "codellb" }
-			for server, config in pairs(servers) do
-				table.insert(ensure_installed, server)
-			end
-			require('mason-lspconfig').setup({
-				ensure_installed,
-				handlers = {
-					function(server_name)
-						require('lspconfig')[server_name].setup(servers[server_name] or {})
-					end,
-					rust_analyzer = lsp.noop,
-				},
-			})
+      require('mason').setup({})
+      local ensure_installed = { "codellb" }
+      for server, config in pairs(servers) do
+        table.insert(ensure_installed, server)
+      end
+      require('mason-lspconfig').setup({
+        ensure_installed,
+        handlers = {
+          function(server_name)
+            require('lspconfig')[server_name].setup(servers[server_name] or {})
+          end,
+          rust_analyzer = lsp.noop,
+        },
+      })
 
-			local cmp = require('cmp')
-			local cmp_action = lsp.cmp_action()
+      local cmp = require('cmp')
+      local cmp_action = lsp.cmp_action()
 
-			cmp.setup({
-				sources = {
-					{ name = "crates", },
-					{ name = "nvim_lsp", group_index = 1, },
-					{ name = "luasnip",  group_index = 2, },
-					{
-						name = "buffer",
-						keyword_length = 2,
-						group_index = 3,
-					},
-					{ name = "path" },
-				},
-				mapping = cmp.mapping.preset.insert(keymaps.cmp(cmp, cmp_action)),
-				performance = {
-					trigger_debounce_time = 500,
-					throttle = 550,
-					fetching_timeout = 80,
-				},
-				preselect = cmp.PreselectMode.Item,
-				sorting = {
-					priority_weight = 2,
-					comparators = {
-						cmp.config.compare.kind,
-						cmp.config.compare.length,
-						cmp.config.compare.offset,
-						cmp.config.compare.recently_used,
-						cmp.config.compare.exact,
-						-- cmp.config.compare.scopes,
-						-- cmp.config.compare.score,
-						-- cmp.config.compare.locality,
-						-- cmp.config.compare.sort_text,
-						-- cmp.config.compare.order,
-					},
-				},
-				-- matching = {
-				-- 	disallow_fuzzy_matching = true,
-				-- 	disallow_fullfuzzy_matching = true,
-				-- 	disallow_partial_fuzzy_matching = true,
-				-- 	disallow_partial_matching = false,
-				-- 	disallow_prefix_unmatching = true,
-				-- },
-				snippet = {
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
-					end,
-				},
-			})
+      cmp.setup({
+        sources = {
+          { name = "crates", },
+          { name = "nvim_lsp", group_index = 1, },
+          { name = "luasnip",  group_index = 2, },
+          {
+            name = "buffer",
+            keyword_length = 2,
+            group_index = 3,
+          },
+          { name = "path" },
+        },
+        mapping = cmp.mapping.preset.insert(keymaps.cmp(cmp, cmp_action)),
+        preselect = cmp.PreselectMode.Item,
+        performance = {
+          debounce = 0,
+          throttle = 0,
+          fetching_timeout = 200,
+          confirm_resolve_timeout = 100,
+          async_budget = 100,
+          max_view_entries = 40,
 
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        },
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            cmp.config.compare.kind,
+            cmp.config.compare.length,
+            cmp.config.compare.offset,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.exact,
+            -- cmp.config.compare.scopes,
+            -- cmp.config.compare.score,
+            -- cmp.config.compare.locality,
+            -- cmp.config.compare.sort_text,
+            -- cmp.config.compare.order,
+          },
+        },
+        -- matching = {
+        -- 	disallow_fuzzy_matching = true,
+        -- 	disallow_fullfuzzy_matching = true,
+        -- 	disallow_partial_fuzzy_matching = true,
+        -- 	disallow_partial_matching = false,
+        -- 	disallow_prefix_unmatching = true,
+        -- },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+      })
+
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 
-			lsp.set_server_config({
-				capabilities = {
-					textDocument = {
-						foldingRange = {
-							dynamicRegistration = false,
-							lineFoldingOnly = true
-						}
-					}
-				}
-			})
+      lsp.set_server_config({
+        capabilities = {
+          textDocument = {
+            foldingRange = {
+              dynamicRegistration = false,
+              lineFoldingOnly = true
+            }
+          }
+        }
+      })
 
-			vim.g.rustaceanvim.server.capabilities = lsp.get_capabilities()
+      vim.g.rustaceanvim.server.capabilities = lsp.get_capabilities()
 
-			vim.o.foldcolumn = '1'
-			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-			vim.o.foldlevelstart = 99
-			vim.o.foldenable = true
-			keymaps.cmp_ufo()
-		end
-	},
+      vim.o.foldcolumn = '1'
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      keymaps.cmp_ufo()
+    end
+  },
 
-	{ 'neovim/nvim-lspconfig' },
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/nvim-cmp' },
-	{ 'L3MON4D3/LuaSnip' },
-	{ 'kevinhwang91/nvim-ufo',            dependencies = 'kevinhwang91/promise-async' },
-	'windwp/nvim-autopairs',
-	'mrcjkb/rustaceanvim',
-	"folke/neodev.nvim"
+  { 'neovim/nvim-lspconfig' },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/nvim-cmp' },
+  { 'L3MON4D3/LuaSnip' },
+  { 'kevinhwang91/nvim-ufo',            dependencies = 'kevinhwang91/promise-async' },
+  'windwp/nvim-autopairs',
+  'mrcjkb/rustaceanvim',
+  "folke/neodev.nvim"
 }
