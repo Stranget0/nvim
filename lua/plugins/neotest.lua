7660809867e576dbde7cbf103e707917db0fad09
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2024-12-15 22:10:50 +0100

    fix neotest adapters

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -11,8 +11,16 @@ return {
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-rust"),
+          require('rustaceanvim.neotest')
         },
+        require('neotest-jest')({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function(path)
+            return vim.fn.getcwd()
+          end,
+        }),
         log_level = vim.log.levels.INFO,
 
         consumers = {
